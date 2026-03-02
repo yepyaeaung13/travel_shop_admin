f3ec61c6449c17065d33006f6ae559f6875aa14e
Ye Pyae Aung <yepyaeaung1311999@gmail.com>
2026-03-03 00:13:16 +0630

    YP: update noti

--- components/notification/notification-container.tsx
+++ components/notification/notification-container.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NotificationItem from "./notification-item";
 import { useGetNoti, useNotiMarkAsAllRead } from "@/queries/noti";
+import { Button } from "../ui/button";
 
 const UnReadNotiContainer = () => {
   const [limit, setLimit] = useState(10);
+  const [list, setList] = useState([]);
   const isSeen = false;
   const { data: notiList, isLoading } = useGetNoti(limit, isSeen);
   const { mutate: markAsAllRead, isPending } = useNotiMarkAsAllRead();
+
+  useEffect(() => {
+    if (notiList?.data) {
+      setList(notiList.data);
+    }
+  }, [notiList]);
 
   const handleMarkAsAllRead = () => {
     markAsAllRead();
@@ -28,20 +36,36 @@ const UnReadNotiContainer = () => {
       </div>
 
       <div className="flex flex-col space-y-4">
-        {isLoading ? (
-          <p className="w-full text-center mt-24">loading...</p>
-        ) : notiList?.data?.length > 0 ? (
-          notiList?.data?.map((n: any) => (
-            <NotificationItem
-              type={n.action}
-              title={n.title}
-              description={n.description}
-              time={n.createdAt}
-              isRead={n.isSeen}
-            />
-          ))
-        ) : (
-          <div className="w-full flex justify-center mt-24">No new notifications.</div>
+        {
+          // isLoading ? (
+          //   <p className="w-full text-center mt-24">loading...</p>
+          // ) :
+          list.length > 0 ? (
+            list.map((n: any) => (
+              <NotificationItem
+                type={n.action}
+                title={n.title}
+                description={n.description}
+                time={n.createdAt}
+                isRead={n.isSeen}
+              />
+            ))
+          ) : !isLoading ? (
+            <div className="w-full flex justify-center mt-24">
+              No new notifications.
+            </div>
+          ) : null
+        }
+
+        {notiList?.data?.length === limit && (
+          <div className="flex w-full justify-center">
+            <Button
+              onClick={() => setLimit((prev) => prev + 10)}
+              className="h-[46px] w-[158px] bg-[#1A1A1A] text-sm font-normal text-white hover:border hover:border-[#1A1A1A] hover:bg-white hover:text-[#1A1A1A] md:w-[240px] md:text-base"
+            >
+              Load More
+            </Button>
+          </div>
         )}
       </div>
     </div>
@@ -50,8 +74,15 @@ const UnReadNotiContainer = () => {
 
 const AllNotiContainer = () => {
   const [limit, setLimit] = useState(10);
+  const [list, setList] = useState([]);
   const { data: notiList, isLoading } = useGetNoti(limit);
   const { mutate: markAsAllRead, isPending } = useNotiMarkAsAllRead();
+
+  useEffect(() => {
+    if (notiList?.data) {
+      setList(notiList.data);
+    }
+  }, [notiList]);
 
   const handleMarkAsAllRead = () => {
     markAsAllRead();
@@ -72,20 +103,35 @@ const AllNotiContainer = () => {
 
       {/* New Notifications  */}
       <div className="flex flex-col space-y-4">
-        {isLoading ? (
-          <p className="w-full text-center mt-24">loading...</p>
-        ) : notiList?.data?.length > 0 ? (
-          notiList?.data?.map((n: any) => (
-            <NotificationItem
-              type={n.action}
-              title={n.title}
-              description={n.description}
-              time={n.createdAt}
-              isRead={n.isSeen}
-            />
-          ))
-        ) : (
-          <div className="w-full flex justify-center mt-24">You don't have any notifications yet.</div>
+        {
+          // isLoading ? (
+          //   <p className="w-full text-center mt-24">loading...</p>
+          // ) :
+          list.length > 0 ? (
+            list.map((n: any) => (
+              <NotificationItem
+                type={n.action}
+                title={n.title}
+                description={n.description}
+                time={n.createdAt}
+                isRead={n.isSeen}
+              />
+            ))
+          ) : !isLoading ? (
+            <div className="w-full flex justify-center mt-24">
+              You don't have any notifications yet.
+            </div>
+          ) : null
+        }
+        {notiList?.data?.length === limit && (
+          <div className="flex w-full justify-center">
+            <Button
+              onClick={() => setLimit((prev) => prev + 10)}
+              className="h-[46px] w-[158px] bg-[#1A1A1A] text-sm font-normal text-white hover:border hover:border-[#1A1A1A] hover:bg-white hover:text-[#1A1A1A] md:w-[240px] md:text-base"
+            >
+              Load More
+            </Button>
+          </div>
         )}
         {/* <div className="h-[1px] w-full bg-[#A1A1A1]/50" /> */}
       </div>
